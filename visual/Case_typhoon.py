4f2105317a1c935630ba06adda805e7bbceb46cd
znwang918 <znwang@bu.edu>
2022-08-28 11:35:03 +0900

    update visual

---
+++ visual/Case_typhoon.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+from copy import copy
+import torch
+from torch import nn, Tensor
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import seaborn as sns
+#sns.axes_style("dark")
+
+
+
+def plot_con_mem(att_with:np.array, att_without:np.array):
+    print(att_with.shape, att_without.shape)
+
+    cmap = copy(plt.cm.plasma)
+    cmap.set_bad(cmap(0))
+
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    ax1.imshow(att_with.transpose(), cmap=cmap, aspect='auto')
+    ax1.set_ylabel('w/', fontsize=15)
+    ax1.set_xticks([x for x in range(0, 12*2*14, 12)])
+    ax1.set_yticks([])
+    ax2.imshow(att_without.transpose(), cmap=cmap, aspect='auto')
+    ax2.set_ylabel('w/o', fontsize=15)
+    ax2.set_xticks([x for x in range(0, 12*2*14, 12)])
+    ax2.set_yticks([])
+
+    dates = ['10-'+str(x).zfill(2) for x in range(7, 21)]
+    labels = []
+    for i in range(len(dates)):
+        labels.append('')
+        labels.append(dates[i])
+    ax2.set_xticklabels(labels, fontsize=13)
+
+    fig.set_size_inches(20, 3)
+    plt.subplots_adjust(bottom=0.3, top=0.7)
+    plt.show()
+
+    return
+
+if __name__ == '__main__':
+
+    # Typhoon outflow
+    # w/ & w/o memory contrastive loss
+    att_with = np.load(f'../model_ours/con_mem/typhoon-outflow_yes_202111092359/MemoryAGCRN_att_score.npy')
+    att_without = np.load(f'../model_ours/con_mem/typhoon-outflow_no_202111161801/MemeSTN_att_score.npy')
+    att_noise1 = np.load(f'../model_ours/con_mem/typhoon-outflow_noise1_202204261154/MemeSTNnoise_att_score.npy')
+    att_noise2 = np.load(f'../model_ours/con_mem/typhoon-outflow_noise2_202204261220/MemeSTNnoise_att_score.npy')
+
+    # test start: 10/06 10AM
+    # +14h start: 10/07 0AM
+    att_with = att_with[14:14+24*14, 5:]
+    att_without = att_without[14:14+24*14, 6:]
+
+    att_noise = att_noise1[14:14+24*14, 7:]
+
+    plot_con_mem(att_with, att_noise)
